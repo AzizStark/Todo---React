fc897573a94ca162115eab90487df92136294788
AzizRahman <rrahman182@gmail.com>
2019-11-30 20:48:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ app.use(bodyParser.json());
 
 app.use('/api', routes);
 
+
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+
+
 app.use((err, req, res, next) => {
   console.log(err);
   next();
